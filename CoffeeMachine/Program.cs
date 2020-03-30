b8b13c1772e9c87a916deafdc3756acccb49fc50
Muhfikri24 <Muhammad.3062@students.amikom.ac.id>
2020-03-30 23:35:49 +0700

    Add project files.

---
+++ CoffeeMachine/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CoffeeMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoffeePowder coffe = new CoffeePowder(100);
+            GalonAir water = new GalonAir(1000);
+            Milk milk = new Milk(300);
+            CoffeeMachine yummyCoffe = new CoffeeMachine(coffe, water, milk);
+
+            //1st cup coffe 
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+            String result = yummyCoffe.makeCappucino();
+            Console.WriteLine("result " + result);
+
+            //2nd cup coffe 
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+            result = yummyCoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            //3nd cup coffe 
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+            result = yummyCoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            //4nd cup coffe 
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+            result = yummyCoffe.makeCappucino();
+            Console.WriteLine("result " + result);
+
+            //5nd cup coffe 
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+            result = yummyCoffe.makeCappucino();
+            Console.WriteLine("result " + result);
+
+            //6nd cup coffe 
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+
+            result = yummyCoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+        }
+    }
+}
